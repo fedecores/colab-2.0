b0effdd21d5cd45e8ec4ab9b613b08bae3713ac2
fedecores <federicocores96@gmail.com>
2025-01-24 23:35:21 -0300

    update

--- Javascript/home.js
+++ Javascript/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#imageModal').on('show.bs.modal', function (event) {
+    $('#imageModal').on('show.bs.modal', function(event) {
         var button = $(event.relatedTarget); // Botón que activó el modal
         var imgSrc = button.data('image'); // Extrae la información de los datos
         var modal = $(this);
@@ -8,11 +8,43 @@ $(document).ready(function() {
 
     // Navbar scroll behavior
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) { // Ajusta el valor según tus necesidades
+        if ($(this).scrollTop() > 30) { // Ajusta el valor según tus necesidades
             $('.navbar').addClass('navbar-fixed');
         } else {
             $('.navbar').removeClass('navbar-fixed');
         }
+
+        // Highlight the current section in the navbar
+        var scrollPos = $(document).scrollTop();
+        $('a.nav-link').each(function() {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top - $('.navbar').outerHeight() <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('a.nav-link').removeClass("active");
+                currLink.addClass("active");
+                currLink.attr('title', 'Estás aquí'); // Añade el tooltip
+            } else {
+                currLink.removeClass("active");
+                currLink.removeAttr('title'); // Elimina el tooltip
+            }
+        });
+    });
+
+    // Smooth scroll with offset for navbar
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            var offset = $('.navbar').outerHeight() ; // Altura del navbar - 50px adicional
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - offset
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+
+            // Colapsar el navbar después de hacer clic en un enlace
+            $('.navbar-collapse').collapse('hide');
+        }
     });
 });
-
